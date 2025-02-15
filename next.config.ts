4ee0dbead04ec31419b9abd219ac0332e97b824d
Ojo Emmanuel T <chincoekun2002@gmail.com>
2025-02-14 20:00:14 -0800

    Update code now

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https', // Protocol of the image source
-        hostname: 'images.unsplash.com', // Hostname of the image source
-        port: '', // Port (optional, usually empty)
-        pathname: '/**', // Path matching pattern (e.g., all paths with `/**`)
-      },
-    ],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
   },
+  images: { unoptimized: true },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
